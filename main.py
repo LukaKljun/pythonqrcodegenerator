577a7e2279908d1fc6fe41e81c45b0cf42279eff
luka kljun <luka.kljun@students.vegova.si>
2024-07-08 21:14:14 +0200

    dela

---
+++ main.py
@@ -0,0 +1,20 @@
+import qrcode
+
+# URL to be encoded in the QR code
+url = "qrcodeapp://?param=HelloWorld"
+
+# Generate QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(url)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill='black', back_color='white')
+
+# Save the image
+img.save("qrcodeapp_qr.png")
